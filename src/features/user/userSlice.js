f2f4091d0a255095a6db3f157c93c27baa985d0b
scKim <lansco1229@gmail.com>
2024-10-25 11:12:15 +0900

    first commit

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -6,26 +6,47 @@ import { initialCart } from "../cart/cartSlice";
 
 export const loginWithEmail = createAsyncThunk(
   "user/loginWithEmail",
-  async ({ email, password }, { rejectWithValue }) => {}
+  async ({ email, password }, { rejectWithValue }) => { }
 );
 
 export const loginWithGoogle = createAsyncThunk(
   "user/loginWithGoogle",
-  async (token, { rejectWithValue }) => {}
+  async (token, { rejectWithValue }) => { }
 );
 
-export const logout = () => (dispatch) => {};
+export const logout = () => (dispatch) => { };
+
 export const registerUser = createAsyncThunk(
   "user/registerUser",
   async (
     { email, name, password, navigate },
     { dispatch, rejectWithValue }
-  ) => {}
+  ) => {
+    try {
+      const response = await api.post("/user", { email, name, password })
+      // 성공
+      // 1. 성공 토스트 메시지 보여주기 
+      // 2. 로그인 페이지 리다이렉트 
+
+      // uiSlice 의 reducer 액션을 호출한다. 
+      dispatch(showToastMessage({ message: "회원 가입을 성공했습니다! ", status: "success" }))
+      navigate('/login')
+
+      return response.data.data     // 값을 리턴을 해야 나중에 Reducer 에서 값을 받을 수 있다. 
+    } catch (error) {
+      // 실패
+      // 1. 실패 토스트 메시지 보여준다. 
+      // 2. 에러값을 저장한다. 
+
+      dispatch(showToastMessage({ message: "회원가입에 실패 했습니다.", status: "error" }))
+      return rejectWithValue(error.error)
+    }
+  }
 );
 
-export const loginWithToken = createAsyncThunk(
+export const loginWithToken = createAsyncThunk(   // 세가지 장점: 상태 세가지 반환, 
   "user/loginWithToken",
-  async (_, { rejectWithValue }) => {}
+  async (_, { rejectWithValue }) => { }
 );
 
 const userSlice = createSlice({
@@ -43,7 +64,19 @@ const userSlice = createSlice({
       state.registrationError = null;
     },
   },
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder.addCase(registerUser.pending, (state) => {
+      state.loading = true;   // loading spinner on 
+
+    })
+      .addCase(registerUser.fulfilled, (state) => {
+        state.loading = false;  // loading spinner off 
+        state.registrationError = null // registration 관련 에러가 있었을 수도 있으니 초기화  
+      })
+      .addCase(registerUser.rejected, (state, action) => {
+        state.registrationError = action.payload
+      })
+  },
 });
 export const { clearErrors } = userSlice.actions;
 export default userSlice.reducer;
